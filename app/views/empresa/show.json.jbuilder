4194d1225cecfaffaf2eb4bbb5e37019931ff8a7
Tarcisiosk <tarcisioskora@hotmail.com>
2015-05-29 14:05:47 -0300

    Initial commit bucket

--- app/views/empresa/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @empresa, :id, :razao_social, :nome_fantasia, :cnpj, :insc_estadual, :insc_municipal, :endereco, :updated_at
--- test/models/conf_tela_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ConfTelaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
